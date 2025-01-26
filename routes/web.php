dbbb69bbe3b3325ba84bcfa75cdf2495379352dd
404Simon <s.wi@mail.de>
2025-01-26 16:36:43 +0100

    refactor: remove unused navbar link and directly redirect to groups

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('groups.index');
 });
 
 Route::get('/dashboard', function () {
@@ -19,13 +19,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('groups', GroupController::class)->except(['show']);  // Exclude 'show' from top-level resource if you moved it above
+
     Route::prefix('groups/{group}')->name('groups.')->group(function () {
         Route::get('/', [GroupController::class, 'show'])->name('show');  // Keep the group show route here if it was removed
-        Route::get('/debts/create', [SharedDebtController::class, 'create'])->name('sharedDebts.create');
+        Route::get('/sharedDebts/create', [SharedDebtController::class, 'create'])->name('sharedDebts.create');
         Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
     });
-
-    Route::resource('groups', GroupController::class)->except(['show']);  // Exclude 'show' from top-level resource if you moved it above
 
     Route::resource('sharedDebts', SharedDebtController::class)
         ->only(['store', 'destroy']);  // Keep only store and destroy for sharedDebts resource
