52066c823cdf71cb73f5a8ac8b8d24acdfffa847
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-02-09 23:13:18 +0100

    refactor: move created by tag

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -19,7 +19,7 @@
                     class="inline-flex items-center justify-center p-2 bg-green-500 hover:bg-green-700 text-white text-sm font-medium rounded-full dark:bg-green-600 dark:hover:bg-green-700">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd"
-                            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 010-2h5V4a1 1 0 011-1z"
                             clip-rule="evenodd" />
                     </svg>
                 </a>
@@ -41,7 +41,7 @@
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
                                 Address
                             </th>
-                            <th scope="col" class="relative px-6 py-3 text-right"> {{-- Align actions to the right --}}
+                            <th scope="col" class="relative px-6 py-3 text-right">
                                 <span class="sr-only">Actions</span>
                             </th>
                         </tr>
@@ -59,10 +59,9 @@
                                 <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">
                                     {{ \Illuminate\Support\Str::limit($marker->address, 50) }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium flex justify-end space-x-2">
-                                    {{-- Align buttons to the right --}}
-                                    @if ($marker->created_by === request()->user()->id)
-                                        <div class="flex space-x-2"> {{-- Keep buttons together and space them --}}
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                    <div class="flex justify-end gap-2">
+                                        @if ($marker->created_by === request()->user()->id)
                                             <a href="{{ route('groups.mapMarkers.show', [$group->id, $marker->id]) }}"
                                                 class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white text-sm font-medium rounded-md dark:bg-blue-600 dark:hover:bg-blue-700">
                                                 View
@@ -71,28 +70,27 @@
                                                 class="inline-flex items-center px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white text-sm font-medium rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700">
                                                 Edit
                                             </a>
-                                        </div>
-                                    @else
-                                        <div class="text-gray-500 text-sm dark:text-gray-400 flex items-center justify-between"
-                                            style="width: 100%;"> {{-- Use justify-between to push text to left --}}
-                                            <div class="text-left"> {{-- Explicitly align text to the left --}}
-                                                Created by: <span
-                                                    class="font-medium dark:text-white">{{ $marker->creator->name }}</span>
+                                        @else
+                                            <div class="flex justify-between items-center w-full">
+                                                <div class="text-gray-500 text-sm dark:text-gray-400">
+                                                    Created by: <span
+                                                        class="font-medium dark:text-white">{{ $marker->creator->name }}</span>
+                                                </div>
+                                                <div class="flex justify-end">
+                                                    <a href="{{ route('groups.mapMarkers.show', [$group->id, $marker->id]) }}"
+                                                        class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white text-sm font-medium rounded-md dark:bg-blue-600 dark:hover:bg-blue-700">
+                                                        View
+                                                    </a>
+                                                </div>
                                             </div>
-                                            <div class="flex justify-end"> {{-- Ensure view button is on the right --}}
-                                                <a href="{{ route('groups.mapMarkers.show', [$group->id, $marker->id]) }}"
-                                                    class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white text-sm font-medium rounded-md dark:bg-blue-600 dark:hover:bg-blue-700">
-                                                    View
-                                                </a>
-                                            </div>
-                                        </div>
-                                    @endif
+                                        @endif
+                                    </div>
                                 </td>
                             </tr>
                         @empty
                             <tr>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400 text-center"
-                                    colspan="4"> {{-- Center the "No map markers" text --}}
+                                    colspan="4">
                                     No map markers added yet.
                                 </td>
                             </tr>
